b20c63d636a2210eebf9b9e4a48a19c5afead13a
Afonso <62111244+AfonsoCruz10@users.noreply.github.com>
2023-03-06 20:18:35 +0000

    Add files via upload

--- generator/src/Sphere.cpp
+++ generator/src/Sphere.cpp
@@ -3,12 +3,12 @@
 #include <cmath>
 #include <math.h>
 
-void Sphere::generateTriangles(int size, int slices, int stacks)
+void Sphere::generateTriangles(int size, int stacks, int slices)
 {
     float radius = size / 2.0;
-    float slicedistance = size * 1.0 / slices;
-    float sliceSize = M_PI / slices;
-    float stackAngle = 2 * M_PI / stacks;
+    float slicedistance = size * 1.0 / stacks;
+    float stacksize = M_PI / stacks;
+    float stackAngle = 2 * M_PI / slices;
     int i, j;
     float y1, y2, sliceRadius1, sliceRadius2, angle;
     Point p1, p2, p3, p4;
@@ -18,7 +18,7 @@ void Sphere::generateTriangles(int size, int slices, int stacks)
     y2 = radius - slicedistance;
     sliceRadius2 = sqrt(pow(radius, 2) - pow(y2, 2));
    
-    for (j = 0; j < stacks; j++){
+    for (j = 0; j < slices; j++){
         p1 = Point(0, radius, 0);
         angle = j * stackAngle;
         p2 = Point(sin(angle) * sliceRadius2, y2, cos(angle) * sliceRadius2);
@@ -38,7 +38,7 @@ void Sphere::generateTriangles(int size, int slices, int stacks)
         addTriangle(t);
     }
     //side of the sphere
-    for (i = 1; i < slices-1; i++) {
+    for (i = 1; i < stacks-1; i++) {
         y1 = radius - i * slicedistance;
         sliceRadius1 = sqrt(pow(radius, 2) - pow(y1, 2));
         i++;
@@ -46,7 +46,7 @@ void Sphere::generateTriangles(int size, int slices, int stacks)
         i--;
         sliceRadius2 = sqrt(pow(radius, 2) - pow(y2, 2));
         
-        for (j = 0; j < stacks; j++)
+        for (j = 0; j < slices; j++)
         {
             angle = j * stackAngle;
             p1 = Point(sin(angle) * sliceRadius1, y1, cos(angle) * sliceRadius1);
